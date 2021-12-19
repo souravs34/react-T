a376664370eb79a2d0575181d434dd02c2ae99bc
Sourav34 <borateourav@gmail.com>
2021-12-19 16:59:51 +0530

    After auth redirecting solved

--- src/app/modules/User/pages/Auth.js
+++ src/app/modules/User/pages/Auth.js
@@ -75,7 +75,7 @@ const Auth = () => {
         });
         const responseData = await sendRequest("users/login", "POST", postData);
 
-        auth.login(responseData.data.user.id);
+        auth.login(responseData.data.userId);
       } catch (err) {}
     } else {
       try {
@@ -96,7 +96,7 @@ const Auth = () => {
           formData
         );
 
-        auth.login(responseData.data.user.id);
+        auth.login(responseData.data.userId);
       } catch (err) {}
     }
   };
