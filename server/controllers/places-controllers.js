03b9d18fb8e9d091aabf37dea044d1bdd5d25f0a
Sourav34 <borateourav@gmail.com>
2021-12-22 22:55:47 +0530

    Delete Authorisation done

--- server/controllers/places-controllers.js
+++ server/controllers/places-controllers.js
@@ -197,6 +197,15 @@ const deletePlace = async (req, res, next) => {
     const error = new HttpError("Could not find place for this id", 404);
     return next(error);
   }
+
+  if (place.creator.id !== req.userData.userId) {
+    const error = new HttpError(
+      "You are not allowed to delete this place.",
+      401
+    );
+    return next(error);
+  }
+
   const imagePath = place.image;
   try {
     // await place.remove();
